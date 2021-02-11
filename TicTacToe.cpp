6f782ad14c2d9b5cd8fdaaf0721f51ff34825c3a
Wayne <wayne.wood@colorado.edu>
2021-02-11 13:57:32 -0700

    DisplayBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -7,7 +7,16 @@ std::vector<std::vector<std::string> > CreateBoard() {
     return new_board;
 }
 
+void DisplayBoard(std::vector<std::vector<std::string> > board) {
+    for (int i=0, m=board.size(); i<m; ++i) {
+        for (int j=0, n=board[i].size(); j<n; ++j) {
+            std::cout << board[i][j] << ", ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main () {
     std::vector<std::vector<std::string> > our_board = CreateBoard();
-    
+
 }
